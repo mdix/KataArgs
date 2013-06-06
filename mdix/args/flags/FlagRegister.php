9425c6a566e53bb0b7db737a19f48379269d62d8
mdix <dix@mediaman.de>
2013-06-06 14:23:00 +0200

    Added FlagRegister test and draft implementation

--- mdix/args/flags/FlagRegister.php
+++ mdix/args/flags/FlagRegister.php
@@ -4,5 +4,22 @@ namespace mdix\args\flags;
 require_once '../../../autoloader.php';
 
 class FlagRegister {
+    private $availableFlagClasses = array();
 
+    public function __construct() {
+        $this->findAvailableFlagClasses();
+    }
+
+    private function findAvailableFlagClasses() {
+        $classnames = scandir(dirname(__FILE__));
+        foreach ($classnames as $classname) {
+            if (preg_match('/FlagKey[A-Za-z]{1}.php/', $classname)) {
+                array_push($this->availableFlagClasses, str_replace('.php', '', $classname));
+            }
+        }
+    }
+
+    public function getAvailableFlagClasses() {
+        return $this->availableFlagClasses;
+    }
 }
---
+++ mdix/args/flags/FlagRegisterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace mdix\args\flags;
+require_once '../../../autoloader.php';
+
+class FlagRegisterTest extends \PHPUnit_Framework_TestCase {
+    private $object;
+
+    public function setUp() {
+        $this->object = new FlagRegister();
+    }
+
+    public function testFindsAllAvailableFlags() {
+        $this->assertCount(3, $this->object->getAvailableFlagClasses());
+    }
+
+    public function testStoresTheWholeClassNamesWithoutExtension() {
+        foreach($this->object->getAvailableFlagClasses() as $className) {
+            $this->assertRegExp('/FlagKey[A-Za-z]{1}$/', $className);
+        }
+    }
+}
